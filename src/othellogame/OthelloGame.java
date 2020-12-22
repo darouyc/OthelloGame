55df19a2d09297fa251bccde6fabf41a0d8f0015
Abdellah1997 <abdellahmetkal97@gmail.com>
2020-12-22 16:13:22 +0100

    Update OthelloGame.java

--- src/othellogame/OthelloGame.java
+++ src/othellogame/OthelloGame.java
@@ -16,7 +16,7 @@ public class OthelloGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HElloooo");
+        System.out.println("Hello World!");
     }
     
 }
